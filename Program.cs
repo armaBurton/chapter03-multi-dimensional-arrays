a2318dbb2a7b867ac6006eef349bb595e29b453a
Arma Burton <arma@armaburton.com>
2023-05-11 23:55:43 -0700

    upper and lower bounds

--- Program.cs
+++ Program.cs
@@ -3,3 +3,8 @@
   {"Anne", "Ben", "Charlie", "Doug"},
   {"Aardvark", "Bear", "Cat", "Dog"}
 };
+
+WriteLine($"Lower bound of the first dimension is {grid1.GetLowerBound(0)}");
+WriteLine($"Upper bound of the first dimension is {grid1.GetUpperBound(0)}");
+WriteLine($"Lower bound of the second dimension is {grid1.GetLowerBound(1)}");
+WriteLine($"Upper bound of the second dimension is {grid1.GetUpperBound(1)}");
