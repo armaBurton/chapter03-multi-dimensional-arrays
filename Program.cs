519fc9d88d35cabe9f36516e7de3f498a1dfe78f
Arma Burton <arma@armaburton.com>
2023-05-11 23:59:13 -0700

    fix format

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ WriteLine($"Upper bound of the second dimension is {grid1.GetUpperBound(1)}");
 
 for (int row = 0; row <= grid1.GetUpperBound(0); row++){
   for (int col = 0; col <= grid1.GetUpperBound(1); col++){
-    WriteLine($"Row {row}, Column {col}: {grid1[row, col
-    ]}");
+    WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
   }
 }
