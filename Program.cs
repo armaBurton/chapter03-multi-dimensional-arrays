e7bd0342a57422a9649026f8361dd0e965942abc
Arma Burton <arma@armaburton.com>
2023-05-11 23:57:52 -0700

    for loop

--- Program.cs
+++ Program.cs
@@ -8,3 +8,10 @@ WriteLine($"Lower bound of the first dimension is {grid1.GetLowerBound(0)}");
 WriteLine($"Upper bound of the first dimension is {grid1.GetUpperBound(0)}");
 WriteLine($"Lower bound of the second dimension is {grid1.GetLowerBound(1)}");
 WriteLine($"Upper bound of the second dimension is {grid1.GetUpperBound(1)}");
+
+for (int row = 0; row <= grid1.GetUpperBound(0); row++){
+  for (int col = 0; col <= grid1.GetUpperBound(1); col++){
+    WriteLine($"Row {row}, Column {col}: {grid1[row, col
+    ]}");
+  }
+}
